4b7ccf283c116f0b6c16971d6dbc2abc860b2233
Daniel Easterman <daniel.easterman@gmail.com>
2018-01-14 18:48:56 +0000

    Profile completed with local environment variable for graphql authorisation setup

--- src/App.js
+++ src/App.js
@@ -1,27 +1,98 @@
 import React, { Component } from 'react';
 import './App.css';
+import { GraphQLClient } from 'graphql-request'
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { text: "" };    
+    this.state = {
+      name: "",
+      url: "",
+      email: "",
+      avatarUrl: "",
+      location: "",
+      websiteUrl: "",
+      error: null
+    };    
   }
 
-  
+  componentDidMount() {    
+
+  const client = new GraphQLClient('https://api.github.com/graphql', {
+    headers: {      
+      Authorization: process.env.REACT_APP_GITHUB_TOKEN,
+    },
+  })
+   
+  const query = `{
+    user(login: "daneasterman") {
+       name
+       url
+       email
+       avatarUrl       
+       location 
+       websiteUrl     
+     }
+  }`
+   
+  client.request(query) 
+    .then(data => {
+      const name = data.user.name
+      const url = data.user.url
+      const email = data.user.email
+      const avatarUrl = data.user.avatarUrl
+      const location = data.user.location
+      const websiteUrl = data.user.websiteUrl
+      // Put this in state
+      this.setState({
+        name: name,
+        url: url,
+        email: email,
+        avatarUrl: avatarUrl,
+        location: location,
+        websiteUrl: websiteUrl
+      })
+      // console.log(email)
+    })
+    .catch(err => {
+      this.setState({error: err})
+    });
+}
+
+renderError() {
+  return (
+    <div>
+      Oops, something went wrong: {this.state.error.message}
+    </div>
+  );
+}
 
   render() {
+
+    const { error, name, url, email, avatarUrl, location, websiteUrl } = this.state;
+
+    if (error) {
+      return this.renderError();
+    }
+
     return (
       <div className="App">
 
-        <header className="App-header">
-          
-          <h1 className="App-title">GraphQL Github Client Example</h1>
-
+        <header className="App-header">          
+          <h1 className="App-title">GraphQL Github API </h1>
         </header>
-        <p className="App-intro">
 
-        </p>
+        <h2 className="App-subtitle">Profile:</h2>
+
+        <p className="App-intro">{name}</p>
+        <p className="App-intro">{url}</p>
+        <p className="App-intro">{email}</p>
+        <img src={avatarUrl} alt="github-profile-avatar"></img>
+        <p className="App-intro">{location}</p>
+        <p className="App-intro">{websiteUrl}</p>
+
+        <h2 className="App-subtitle">Repositories:</h2>
 
       </div>
     );
